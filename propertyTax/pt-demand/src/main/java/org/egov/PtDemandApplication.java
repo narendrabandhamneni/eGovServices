88272eef7070980646e9492c5beff226cd79ac95
Supraja <supraja.rachamalla@walkingtree.in>
2017-05-09 15:07:45 +0530

    adding projects to git

---
+++ propertyTax/pt-demand/src/main/java/org/egov/PtDemandApplication.java
@@ -0,0 +1,12 @@
+package org.egov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtDemandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtDemandApplication.class, args);
+	}
+}
---
+++ propertyTax/pt-userValidator/src/main/java/org/egov/PtUserValidatorApplication.java
@@ -0,0 +1,12 @@
+package org.egov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtUserValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtUserValidatorApplication.class, args);
+	}
+}
