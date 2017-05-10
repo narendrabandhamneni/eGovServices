b3e57df3eec26e2aca0df4513747435aefeba542
PavanWTC-PC\PavanWTC <pavan.kumar@walkingtree.tech>
2017-05-10 19:12:37 +0530

    committing the uservalidator producer, consumer, model

--- propertyTax/pt-userValidator/src/main/java/org/egov/propertyUser/model/UserModel.java
+++ propertyTax/pt-userValidator/src/main/java/org/egov/propertyUser/model/UserModel.java
@@ -1,5 +1,95 @@
 package org.egov.propertyUser.model;
 
 public class UserModel {
+	
+	private long id;
+	
+	private String userName;
+	
+	private String name;
+	
+	private String gender;
+	
+	private String mobileNumber;
+	
+	private Boolean active;
+	
+	private String locale;
+	
+	private String type;
+	
+	private String tenantId;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public String getLocale() {
+		return locale;
+	}
+
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getTenantId() {
+		return tenantId;
+	}
+
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
 
 }
