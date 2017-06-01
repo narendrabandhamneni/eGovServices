d881bff4c87852dfb06c63c27e9019a0182f30b9
PavanWTC-PC\PavanWTC <pavan.kumar@walkingtree.tech>
2017-06-01 19:38:53 +0530

    committing the changes of the idgeneration service changes

--- propertyTax/pt-idGeneration/src/main/java/org/egov/id/PtIdGenerationApplication.java
+++ propertyTax/pt-idGeneration/src/main/java/org/egov/id/PtIdGenerationApplication.java
@@ -1,12 +1,19 @@
 package org.egov.id;
 
+import org.egov.models.ResponseInfoFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PtIdGenerationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PtIdGenerationApplication.class, args);
+	}
+	
+	@Bean
+	public ResponseInfoFactory getResponseInfo(){
+		return new ResponseInfoFactory();
 	}
 }
