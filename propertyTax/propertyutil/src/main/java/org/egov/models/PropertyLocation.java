0cdca8394354ee066e90a212c98dac0f67bb0a96
PavanWTC-PC\PavanWTC <pavan.kumar@walkingtree.tech>
2017-06-05 17:44:01 +0530

    committing the changes of the boundary validator changes

--- propertyTax/propertyutil/src/main/java/org/egov/models/PropertyLocation.java
+++ propertyTax/propertyutil/src/main/java/org/egov/models/PropertyLocation.java
@@ -1,5 +1,9 @@
 package org.egov.models;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
@@ -32,5 +36,22 @@ public class PropertyLocation {
 	
 	@Size(min=1, max=256)
 	private String southBoundedBy;
+	
+	public List<String> getFieldsOfType(Class<?> target, Class<?> searchType) {
+
+        Field[] fields  = target.getDeclaredFields();
+        List<String> result = new ArrayList<String>();
+        
+        for(Field field:fields){
+            if(field.getType().equals(searchType)){
+            	result.add(searchType.getName());
+            }
+        }
+        return result;
+    }
+
+    public List<String> getAllBoundaries(){
+        return getFieldsOfType(PropertyLocation.class, Boundary.class); 
+    }
 	
 }
--- propertyTax/pt-property/src/main/java/org/egov/property/util/PropertyValidator.java
+++ propertyTax/pt-property/src/main/java/org/egov/property/util/PropertyValidator.java
@@ -1,8 +1,10 @@
 package org.egov.property.util;
 
 import java.net.URI;
+import java.util.List;
 
 import org.egov.models.Property;
+import org.egov.models.PropertyLocation;
 import org.egov.property.exception.InvalidPropertyBoundaryException;
 import org.egov.property.model.BoundaryResponseInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,19 +16,38 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class PropertyValidator {
-	
+
 	@Autowired
 	private Environment env;
 
 	@Autowired
 	RestTemplate restTemplate;
+
+	public void validatePropertyBoundary(Property property) throws InvalidPropertyBoundaryException{
+
+		PropertyLocation propertyLocation = property.getBoundary();
+		List<String> fields = propertyLocation.getAllBoundaries();
+		for(String field:fields){
+			validateBoundaryFields(property, field);
+		}
+	}
 	
-	public Boolean validatePropertyBoundary(Property property) throws InvalidPropertyBoundaryException{
+	public Boolean validateBoundaryFields(Property property, String field) throws InvalidPropertyBoundaryException{
+		
+		PropertyLocation propertyLocation = property.getBoundary();
+		String id;
+		if(field.equalsIgnoreCase("revenueBoundary")){
+			id = propertyLocation.getRevenueBoundary().getId();
+		} else if(field.equalsIgnoreCase("locationBoundary")){
+			id = propertyLocation.getLocationBoundary().getId();
+		} else {
+			id = propertyLocation.getAdminBoundary().getId();
+		}
 		
 		URI uri = UriComponentsBuilder.fromUriString(env.getProperty("boundary.boundaryUrl"))
-				.queryParam("Boundary.tenantId", property.getBoundary().getTenantId())
-				.queryParam("Boundary.id", property.getBoundary().getId()).build(true).encode().toUri();
-		
+				.queryParam("Boundary.tenantId", property.getTenantId())
+				.queryParam("Boundary.id", id).build(true).encode().toUri();
+
 		try {
 			BoundaryResponseInfo boundaryResponseInfo = restTemplate.getForObject(uri, BoundaryResponseInfo.class);
 			if(boundaryResponseInfo.getResponseInfo().getStatus().equalsIgnoreCase(env.getProperty("statusCode")) && boundaryResponseInfo.getBoundary().size()!=0){
@@ -35,11 +56,7 @@ public class PropertyValidator {
 				throw new InvalidPropertyBoundaryException();
 			}
 		} catch (HttpClientErrorException ex){
-          throw new InvalidPropertyBoundaryException();
+			throw new InvalidPropertyBoundaryException();
 		}
-	}
-	
-	public Boolean validateProperty(Property property){
-		return validatePropertyBoundary(property);
 	}
 }
