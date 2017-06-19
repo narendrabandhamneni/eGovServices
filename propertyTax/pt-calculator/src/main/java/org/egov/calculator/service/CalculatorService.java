2c0b1019fcbeb7f1b5a36475ce4e47fdd7168d75
PavanWTC-PC\PavanWTC <pavan.kumar@walkingtree.tech>
2017-06-19 20:05:37 +0530

    committing the changes of the propertytax calculator

---
+++ propertyTax/pt-calculator/src/main/java/org/egov/calculator/service/CalculatorService.java
@@ -0,0 +1,57 @@
+package org.egov.calculator.service;
+
+import org.egov.models.CalculationFactorRequest;
+import org.egov.models.CalculationFactorResponse;
+import org.egov.models.GuidanceValueRequest;
+import org.egov.models.GuidanceValueResponse;
+import org.egov.models.TaxRatesRequest;
+import org.egov.models.TaxRatesResponse;
+import org.egov.models.TaxPeriodRequest;
+import org.egov.models.TaxPeriodResponse;
+import org.egov.models.RequestInfo;
+
+public interface CalculatorService {
+
+	// factor
+	public CalculationFactorResponse createFactor(String tenantId,
+			CalculationFactorRequest calculationFactorRequest) throws Exception;
+
+	public CalculationFactorResponse updateFactor(String tenantId,
+			CalculationFactorRequest calculationFactorRequest) throws Exception;
+
+	public CalculationFactorResponse getFactor(RequestInfo requestInfo,
+			String tenantId, String factorType, String validDate, String code)
+			throws Exception;
+
+	// guidancevalue
+	public GuidanceValueResponse createGuidanceValue(String tenantId,
+			GuidanceValueRequest guidanceValueRequest) throws Exception;
+	
+	public GuidanceValueResponse updateGuidanceValue(String tenantId,
+			GuidanceValueRequest guidanceValueRequest) throws Exception;
+	
+	public GuidanceValueResponse getGuidanceValue(RequestInfo requestInfo,
+		   String tenantId, String boundary, String structure, String usage, String subUsage, 
+		   String occupancy, String validDate, String code) throws Exception;
+	
+	// taxrates
+	public TaxRatesResponse createTaxRate(String tenantId,
+			TaxRatesRequest taxRatesRequest) throws Exception;
+	
+	public TaxRatesResponse updateTaxRate(String tenantId,
+			TaxRatesRequest taxRatesRequest) throws Exception;
+	
+	public TaxRatesResponse getTaxRate(RequestInfo requestInfo,
+		   String tenantId, String taxHead, String validDate, String parentTaxHead) throws Exception;
+
+	// taxperiod
+	public TaxPeriodResponse createTaxPeriod(String tenantId,
+			TaxPeriodRequest taxPeriodRequest) throws Exception;
+	
+	public TaxPeriodResponse updateTaxPeriod(String tenantId,
+			TaxPeriodRequest taxPeriodRequest) throws Exception;
+	
+	public TaxPeriodResponse getTaxPeriod(RequestInfo requestInfo,
+		   String tenantId, String validDate, String code) throws Exception;
+
+}
---
+++ propertyTax/pt-calculator/src/main/java/org/egov/calculator/service/CalculatorServiceImpl.java
@@ -0,0 +1,111 @@
+package org.egov.calculator.service;
+
+import org.egov.models.CalculationFactorRequest;
+import org.egov.models.CalculationFactorResponse;
+import org.egov.models.GuidanceValueRequest;
+import org.egov.models.GuidanceValueResponse;
+import org.egov.models.TaxRatesRequest;
+import org.egov.models.TaxRatesResponse;
+import org.egov.models.TaxPeriodRequest;
+import org.egov.models.TaxPeriodResponse;
+import org.egov.models.RequestInfo;
+import org.springframework.stereotype.Service;
+
+/**
+ * Description : CalculatorService interface implementation class
+ * 
+ * @author Pavan Kumar Kamma
+ *
+ */
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+
+	@Override
+	public CalculationFactorResponse createFactor(String tenantId,
+			CalculationFactorRequest calculationFactorRequest)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CalculationFactorResponse updateFactor(String tenantId,
+			CalculationFactorRequest calculationFactorRequest)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CalculationFactorResponse getFactor(RequestInfo requestInfo,
+			String tenantId, String factorType, String validDate, String code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GuidanceValueResponse createGuidanceValue(String tenantId,
+			GuidanceValueRequest guidanceValueRequest) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public GuidanceValueResponse updateGuidanceValue(String tenantId,
+			GuidanceValueRequest guidanceValueRequest) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public GuidanceValueResponse getGuidanceValue(RequestInfo requestInfo,
+			   String tenantId, String boundary, String structure, String usage, String subUsage, 
+			   String occupancy, String validDate, String code) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public TaxRatesResponse createTaxRate(String tenantId,
+			TaxRatesRequest taxRatesRequest) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	};
+	
+	@Override
+	public TaxRatesResponse updateTaxRate(String tenantId,
+			TaxRatesRequest taxRatesRequest) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	};
+	
+	@Override
+	public TaxRatesResponse getTaxRate(RequestInfo requestInfo,
+		   String tenantId, String taxHead, String validDate, String parentTaxHead) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	};
+	
+	@Override
+	public TaxPeriodResponse createTaxPeriod(String tenantId,
+			TaxPeriodRequest taxPeriodRequest) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	};
+	
+	@Override
+	public TaxPeriodResponse updateTaxPeriod(String tenantId,
+			TaxPeriodRequest taxPeriodRequest) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	};
+	
+	@Override
+	public TaxPeriodResponse getTaxPeriod(RequestInfo requestInfo,
+		   String tenantId, String validDate, String code) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	};
+
+}
